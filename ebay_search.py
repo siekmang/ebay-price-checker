e56108fc3bfef92c423276968d3c515c81d52b55
grsiekman <greg@siekmang.com>
2026-01-10 21:22:16 -0500

    Changed launch_firefox to ebay_search
    Added comments for code navigability

--- ebay_search.py
+++ ebay_search.py
@@ -6,36 +6,48 @@ from selenium.webdriver.support.wait import WebDriverWait
 '''TODO; Handle errors and exceptions gracefully'''
 '''TODO; Messages to let the user know what is happening'''
 
-def launch_firefox():
+
+def ebay_search():
+    """Launch Firefox and perform eBay search for Nintendo Switch 2 Sealed in Box"""
+    '''Initialize and launch Firefox with headless mode'''
     options = webdriver.FirefoxOptions()
     options.add_argument('--headless')
     selenium = webdriver.Firefox(options=options)
+
+    '''Navigate to eBay and perform search'''
     selenium.get('https://www.ebay.com')
     search_bar = selenium.find_element(By.ID, 'gh-ac')
     search_bar.send_keys('Nintendo Switch 2 Sealed in Box')
     search_bar.submit()
 
+    '''Introduce wait'''
     wait = WebDriverWait(selenium, 10)
+
+    '''Click on Video Game Consoles'''
     video_game_console_locator = (By.XPATH, "//a[contains(., 'Video Game Consoles')]")
     video_game_console_element = wait.until(ec.element_to_be_clickable(video_game_console_locator))
 
     video_game_console_element.click()
 
+    '''Click on Buy It Now'''
     buy_it_now_locator = (By.XPATH, "//a[contains(., 'Buy It Now')]")
     buy_it_now_button = wait.until(ec.element_to_be_clickable(buy_it_now_locator))
 
     buy_it_now_button.click()
 
+    '''Click on Free Shipping'''
     free_shipping_locator = (By.XPATH, "//span[contains(., 'Free Shipping')]")
     free_shipping_element = wait.until(ec.presence_of_element_located(free_shipping_locator))
 
     free_shipping_element.click()
 
+    '''Click on New'''
     new_locator = (By.XPATH, "//span[contains(., 'New')]")
     new_element = wait.until(ec.presence_of_element_located(new_locator))
 
     new_element.click()
 
+    '''Sort by Price'''
     sort_locator = (By.XPATH, "//span[contains(., 'Sort')]")
     sort_element = wait.until(ec.presence_of_element_located(sort_locator))
 
@@ -46,13 +58,16 @@ def launch_firefox():
 
     sort_by_price_element.click()
 
+    '''Get results'''
     results = selenium.find_elements(By.XPATH, "//li[contains(@class, 's-card--horizontal')]")
 
     for i in range(2, 5):
-        wait.until(ec.presence_of_element_located((By.XPATH, ".//span[contains(@class, 'su-styled-text primary default')]")))
+        wait.until(
+            ec.presence_of_element_located((By.XPATH, ".//span[contains(@class, 'su-styled-text primary default')]")))
         title_element = results[i].find_element(By.XPATH, ".//span[contains(@class, 'su-styled-text primary default')]")
 
-        price_elements = results[i].find_elements(By.XPATH, ".//span[@class='su-styled-text primary bold large-1 s-card__price']")
+        price_elements = results[i].find_elements(By.XPATH,
+                                                  ".//span[@class='su-styled-text primary bold large-1 s-card__price']")
         result_price_text = price_elements[0].text if price_elements else "Price not found"
 
         link_element = results[i].find_element(By.XPATH, ".//a[contains(@class, 's-card__link')]")
@@ -60,6 +75,7 @@ def launch_firefox():
         max_len = 75
         truncated_link = (link_url[:max_len] + '...') if len(link_url) > max_len else link_url
 
+        '''Print results'''
         print(f"Title: {title_element.text}")
         print(f"Price: {result_price_text}")
         print(f"Link: {truncated_link}")
@@ -67,8 +83,9 @@ def launch_firefox():
 
     selenium.quit()
 
+
 if __name__ == '__main__':
     try:
-        launch_firefox()
+        ebay_search()
     except Exception as e:
         print(f"An error occurred: {e}")
